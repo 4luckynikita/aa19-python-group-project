e877971a034a42654d23f0c5af65a64d4190de0b
Cece Potakey <cecepotakey24@gmail.com>
2024-05-20 20:08:04 -0400

    get all reviews backend route tested and functioning

--- app/api/reviews_routes.py
+++ app/api/reviews_routes.py
@@ -3,3 +3,13 @@ from flask_login import login_required
 from app.models import Review
 
 review_routes = Blueprint('reviews', __name__)
+
+# get all reviews
+@review_routes.route('/')
+@login_required
+def reviews():
+    """
+    Query for all reviews and returns them in a list of dictionaries
+    """
+    reviews = Review.query.all()
+    return [review.to_dict() for review in reviews]
