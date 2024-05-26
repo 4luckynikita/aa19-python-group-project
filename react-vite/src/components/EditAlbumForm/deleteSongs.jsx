4ed81f89d6e8fe71fe5f899017691cdf8904b125
Cece Potakey <cecepotakey24@gmail.com>
2024-05-25 22:10:15 -0400

    stashed changes

---
+++ react-vite/src/components/EditAlbumForm/deleteSongs.jsx
@@ -0,0 +1,44 @@
+import { MdDelete } from "react-icons/md";
+import { deleteSong, fetchAlbums, fetchCurrentAlbum } from "../../redux/albums";
+import { useDispatch, useSelector } from "react-redux";
+
+
+
+function DeleteSong({songsArray, id}) {
+    const currentUser = useSelector((state) => state.session.user);
+    const dispatch = useDispatch()
+    const handleDelete = async (songId, title) => {
+
+        alert(`you have deleted the song titled ${title}`)
+        const deletedSong = await dispatch(deleteSong(songId, currentUser.id));
+        if (deletedSong) {
+          dispatch(fetchAlbums(currentUser.id))
+          dispatch(fetchCurrentAlbum(id))
+        }
+      };
+
+
+
+
+    return (
+        <>
+            {songsArray &&
+                (<ol >
+                    {songsArray.map((song) => (
+                        <li key={song.id}>
+                            {song.title}
+                            <MdDelete
+                                onClick={(e) => {
+                                    e.preventDefault();
+                                    handleDelete(song.id, song.title);
+                                }}
+                            />
+                        </li>
+                    ))}
+                </ol>)
+            }
+        </>
+    )
+}
+
+export default DeleteSong
